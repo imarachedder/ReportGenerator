5db9ea9f7cf8a02ce01fa6cbd90ce7cba26668db
Qu0l0 <burchik.99@mail.ru>
2024-02-26 11:59:37 +0600

    дописал get_dada_datas для получения данных из датасета

--- db.py
+++ db.py
@@ -371,96 +371,98 @@ def sort_dict_binding (res):
         if not isinstance(value, dict):
             continue
         for val in value.values():
-            #if isinstance(value, dict):
+            # if isinstance(value, dict):
             for elem in val.values():
-                 elem.sort(key = lambda x: (x[-2], x[-1]))
+                elem.sort(key = lambda x: (x[-2], x[-1]))
 
-def convert_m_to_km ( param, list_km):
-        '''
+
+def convert_m_to_km (param, list_km):
+    '''
         переводит метры в километры с привязкой к километровым знакам
         :param param: строка из базы данных
         :param list_km: список километровых знаков
         :return: привязки начала и конца объекта
         '''
-        # частный случай километровые знаки
-        if param[2] == 'Километровые знаки':
-            #('2', 0.0, 0.0, 1, 1, 4.238, 4.238, 2000, 2000, (0, 0), (0, 0))
-            return (int(param[4]), 0), (int(param[4]), 0)
-        # если точечный объект
-        if param[-2] == param[-1]:
-            # print('start==end')
+    # частный случай километровые знаки
+    if param[2] == 'Километровые знаки':
+        # ('2', 0.0, 0.0, 1, 1, 4.238, 4.238, 2000, 2000, (0, 0), (0, 0))
+        return (int(param[4]), 0), (int(param[4]), 0)
+    # если точечный объект
+    if param[-2] == param[-1]:
+        # print('start==end')
 
-            for idx_km, num_sign in enumerate(list_km):
-                # находим следующий километровый
-                if num_sign == list_km[-1]:
-                    next_km = list_km[-1]
-                elif num_sign == list_km[0]:
-                    next_km = list_km[1]
-                else:
-                    next_km = list_km[idx_km % len(list_km) + 1]
+        for idx_km, num_sign in enumerate(list_km):
+            # находим следующий километровый
+            if num_sign == list_km[-1]:
+                next_km = list_km[-1]
+            elif num_sign == list_km[0]:
+                next_km = list_km[1]
+            else:
+                next_km = list_km[idx_km % len(list_km) + 1]
 
-                if num_sign[-2] <= param[-2] < next_km[-2]:
-                    return (int(num_sign[0]), param[-2] - num_sign[-2]), (int(num_sign[0]), param[-1] - num_sign[-1])
-                elif param[-2] >= list_km[-1][-2]:
-                    return (int(list_km[-1][0]), param[-2] - list_km[-1][-2]), (
-                        int(list_km[-1][0]), param[-1] - list_km[-1][-1])
-                # elif tmp[-2] <= param[-2] < num_sign[-2]:
-                #     return (tmp[0], param[-2] - tmp[-2]), (tmp[0], param[-1] - tmp[-1])
-                elif param[-2] < num_sign[-2]:
-                    return (int(num_sign[0]), param[-2] - num_sign[-2]), (int(num_sign[0]), param[-1] - num_sign[-1])
-                # elif next_km[-2] <= param[-2]:
-                #     continue
-                #     # return (next_km[0], param[-2] - next_km[-2]), (next_km[0], param[-1] - next_km[-1])
-                else:
-                    continue
-        # если объект линейный или площадной
-        elif param[-2] != param[-1]:
-            start_km = 0
-            end_km = 0
-            start_m = 0  # начало
-            end_m = 0  # конец
-            idx = 0  # индекс километрового start
-            # ищем start
-            for idx_km, num_sign in enumerate(list_km):
-                if num_sign == list_km[-1]:
-                    next_km = list_km[-1]
-                    #idx = idx_km
-                elif num_sign == list_km[0]:
-                    next_km = list_km[1]
-                    #idx = idx_km
-                else:
-                    next_km = list_km[idx_km % len(list_km) + 1]
-                idx = idx_km
-                if num_sign[-2] <= param[-2] < next_km[-2] or param[-2] < num_sign[-2]:
-                    start_km = num_sign[0]
-                    start_m = param[-2] - num_sign[-2]
-                    break
-                elif param[-2] > list_km[-1][-2]:
-                    start_km = list_km[-1][0]
-                    start_m = param[-2] - list_km[-1][-2]
-                    break
-                else:
-                    continue
-            # ищем end начиная с idx благодаря этому отсекаются уже пройденные километровые
-            for num_sign in list_km[idx:]:
-                if num_sign == list_km[-1]:
-                    next_km = list_km[-1]
-                elif num_sign == list_km[0]:
-                    next_km = list_km[1]
-                else:
-                    next_km = list_km[idx % len(list_km) + 1]
-                if num_sign[-1] <= param[-1] < next_km[-1] or param[-1] < num_sign[-1]:
-                    end_km = num_sign[0]
-                    end_m = param[-1] - num_sign[-1]
-                    break
-                elif param[-1] > list_km[-1][-1]:
-                    end_km = list_km[-1][0]
-                    end_m = param[-1] - list_km[-1][-1]
-                    break
+            if num_sign[-2] <= param[-2] < next_km[-2]:
+                return (int(num_sign[0]), param[-2] - num_sign[-2]), (int(num_sign[0]), param[-1] - num_sign[-1])
+            elif param[-2] >= list_km[-1][-2]:
+                return (int(list_km[-1][0]), param[-2] - list_km[-1][-2]), (
+                    int(list_km[-1][0]), param[-1] - list_km[-1][-1])
+            # elif tmp[-2] <= param[-2] < num_sign[-2]:
+            #     return (tmp[0], param[-2] - tmp[-2]), (tmp[0], param[-1] - tmp[-1])
+            elif param[-2] < num_sign[-2]:
+                return (int(num_sign[0]), param[-2] - num_sign[-2]), (int(num_sign[0]), param[-1] - num_sign[-1])
+            # elif next_km[-2] <= param[-2]:
+            #     continue
+            #     # return (next_km[0], param[-2] - next_km[-2]), (next_km[0], param[-1] - next_km[-1])
+            else:
+                continue
+    # если объект линейный или площадной
+    elif param[-2] != param[-1]:
+        start_km = 0
+        end_km = 0
+        start_m = 0  # начало
+        end_m = 0  # конец
+        idx = 0  # индекс километрового start
+        # ищем start
+        for idx_km, num_sign in enumerate(list_km):
+            if num_sign == list_km[-1]:
+                next_km = list_km[-1]
+                # idx = idx_km
+            elif num_sign == list_km[0]:
+                next_km = list_km[1]
+                # idx = idx_km
+            else:
+                next_km = list_km[idx_km % len(list_km) + 1]
+            idx = idx_km
+            if num_sign[-2] <= param[-2] < next_km[-2] or param[-2] < num_sign[-2]:
+                start_km = num_sign[0]
+                start_m = param[-2] - num_sign[-2]
+                break
+            elif param[-2] > list_km[-1][-2]:
+                start_km = list_km[-1][0]
+                start_m = param[-2] - list_km[-1][-2]
+                break
+            else:
+                continue
+        # ищем end начиная с idx благодаря этому отсекаются уже пройденные километровые
+        for num_sign in list_km[idx:]:
+            if num_sign == list_km[-1]:
+                next_km = list_km[-1]
+            elif num_sign == list_km[0]:
+                next_km = list_km[1]
+            else:
+                next_km = list_km[idx % len(list_km) + 1]
+            if num_sign[-1] <= param[-1] < next_km[-1] or param[-1] < num_sign[-1]:
+                end_km = num_sign[0]
+                end_m = param[-1] - num_sign[-1]
+                break
+            elif param[-1] > list_km[-1][-1]:
+                end_km = list_km[-1][0]
+                end_m = param[-1] - list_km[-1][-1]
+                break
 
-                else:
-                    continue
-            return (int(start_km), start_m), (int(end_km), end_m)
+            else:
+                continue
+        return (int(start_km), start_m), (int(end_km), end_m)
+
 
 class Query:
     def __init__ (self, database = None):
@@ -520,6 +522,7 @@ class Query:
 
         sort_dict_binding(res_km)
         return res_km
+
     def get_tp_datas (self, road_name):
         """
          Последовательность объектов:
@@ -530,6 +533,7 @@ class Query:
             максимальное отклонение, начало_метры, конец_метры, (начало_километры,начало_метры),
             (конец_километры, конец_метры)), ....], ...}, ...}, ...}
         :return:
+
         """
 
         res_km = self.get_km_sign_list(road_name)
@@ -586,7 +590,7 @@ class Query:
         self.cursor.execute(request, road_name)  # item
 
         for i, param in enumerate(self.cursor.fetchall()):
-            icecream.ic(i, param)
+            #icecream.ic(i, param)
             tuple_km = tuple(
                 res_km.get(param[1], {}).get('Километровые знаки', {}).get('Значение в прямом направлении', []))
 
@@ -611,97 +615,66 @@ class Query:
 
         # сортирует в словаре координаты по возрастанию
         sort_dict_binding(res)
-        #print(res)
+        # print(res)
         return dict(sorted(res.items()))
 
-    def get_dad_datas (self, name):
-        """ 29.06.2023 """
+    def get_dad_datas (self, road_name):
         """
-        Вытаскиваем данные из бд и формируем словарь по атрибутам
-        :param attr_list: хранит в себе названия атрибутов
+         Вытаскиваем данные из бд и формируем словарь по атрибутам
         :param name: название автомобильной дороги 
         :return: возвращаем словарь с данными по диагностике
         """
-        dad_datas_dict = {}
-
-        attr_list = [
-            'Оценка ровности IRI', 'Глубина колеи', 'Граница участка дороги', 'Ось дороги', 'Кривая',
-            'Ширина проезжей части'
-        ]
 
         request = """
-            select DISTINCT  High.Description, Group_Description.Item_Name, Types_Description.Param_Name,
-             Params.ValueParam, Attribute.L1, Attribute.L2
-            from Road 
-            inner join Way on Road.ID_Road = Way.ID_Way
-            inner join High on Way.ID_Way = High.ID_Way
-            inner join Attribute on High.ID_High = Attribute.ID_High 
-            inner join Params on Attribute.ID_Attribute = Params.ID_Attribute
-            inner join Types_Description on Params.ID_Param = Types_Description.ID_Param
-            inner join Group_Description on Attribute.ID_Type_Attr = Group_Description.ID_Type_Attr
-            where Road.Name = ? and Group_Description.Item_Name = 'Ось дороги' or Group_Description.Item_Name = 'Граница участка дороги'
-                       or Group_Description.Item_Name = 'Километровые знаки' or Group_Description.Item_Name = 'Кривая' or Group_Description.Item_Name = 'Оценка ровности IRI'
-                       or Group_Description.Item_Name  = 'Глубина колеи'   or Group_Description.Item_Name  = 'Ширина проезжей части'    
-        """
-        request_test = """
-                      select DISTINCT High.Description, Group_Description.Item_Name,Params.ValueParam, Attribute.L1, Attribute.L2
-                      from Road 
-                      inner join (select ID_Way from Way )as Way on Road.ID_Road = Way.ID_Way
-                      inner join (select ID_Way ,ID_High, Description from High)as High on Way.ID_Way = High.ID_Way
-                      inner join (select ID_High,ID_Attribute,ID_Type_Attr,L1,L2 from Attribute) as Attribute on High.ID_High = Attribute.ID_High 
-                      inner join (select ID_Type_Attr,Item_Name from Group_Description) as Group_Description on Attribute.ID_Type_Attr = Group_Description.ID_Type_Attr
-                      inner join (select ID_Attribute,ValueParam from Params )as Params on Attribute.ID_Attribute = Params.ID_Attribute
-                      where Road.Name = ? and Group_Description.Item_Name = 'Ось дороги' or Group_Description.Item_Name = 'Граница участка дороги'
-                       or Group_Description.Item_Name = 'Километровые знаки' or Group_Description.Item_Name = 'Кривая' or Group_Description.Item_Name = 'Оценка ровности IRI'
-                       or Group_Description.Item_Name  = 'Глубина колеи'   or Group_Description.Item_Name  = 'Ширина проезжей части'
- 
-            
-                """
-        # and Group_Description.Item_Name = 'Ось дороги' or Group_Description.Item_Name =
-        # 'Граница участка дороги' or Group_Description.Item_Name = 'Километровые знаки'
+            select DISTINCT High.Description, Group_Description.Item_Name, Types_Description.Param_Name,
+            Params.ValueParam, Attribute.L1, Attribute.L2
+                    from Road inner join Way on Road.ID_Road = Way.ID_Road
+                    inner join High on Way.ID_Way = High.ID_Way
+                    inner join Attribute on High.ID_High = Attribute.ID_High
+                    inner join Params on Attribute.ID_Attribute = Params.ID_Attribute
+                    inner join Types_Description on Params.ID_Param = Types_Description.ID_Param
+                    inner join Group_Description on Types_Description.ID_Type_Attr = Group_Description.ID_Type_Attr
+                    where Road.Name = ? and Group_Description.Item_Name in ( 'Ось дороги' ,
+                                                  'Километровые знаки' ,
+                                                  'Граница участка дороги',
+                                                  'Кривая' ,
+                                                  'Оценка ровности IRI',
+                                                  'Глубина колеи' ,
+                                                  'Ширина проезжей части'  ,
+                                                  'Ширина земляного полотна',
+                                                  'Ширина обочин',
+                                                  'Оценка состояния покрытия (баллы)',
+                                                  'Коэффициент сцепления',
+                                                  'Модуль упругости')
+              """
+        res_km = self.get_km_sign_list(road_name)
+        self.cursor.execute(request, road_name)
 
-        self.cursor.execute(request, name)
-       #for num, attr in enumerate(attr_list):
-       #    icecream.ic('='*20, attr, '='*20)
-            # datas_dict = {
-            #     'Name': [], 'Description_uch1': [], 'L1-Начало': [], 'L2-Конец': [],
-            #     'ID_Param': [], 'ValueParam': [], 'ID_Type_Attr': [], 'Item_Name': []
-            # }
-        res_km = {}
-        for i, param in enumerate(self.cursor.fetchmany(5)):
-            """ заполяяем список данными из базы """
+        res = {'название дороги': road_name}
+        for i, param in enumerate(self.cursor.fetchall()):
+            #icecream.ic(i, param)
+            tuple_km = tuple(
+                res_km.get(param[0], {}).get('Километровые знаки', {}).get('Значение в прямом направлении', []))
 
-            icecream.ic('DAD',i,param)
-            if param[0] in res_km:
-                if param[1] in res_km.get(param[0]):
-                    if param[2] in res_km.get(param[0]).get(param[1]):
-                        res_km.get(param[0]).get(param[1]).get(param[2]).append(param[3:])
-                    else:
-                        res_km.get(param[0]).get(param[1]).update({param[2]: [param[3::]]})
-                else:
-                    res_km.get(param[0]).update({param[1]: {param[2]: [param[3::]]}})
+            if tuple_km:
+                km = convert_m_to_km(param, tuple_km)
             else:
-                res_km.update({param[0]: {param[1]: {param[2]: [param[3::]]}}})
-        icecream.ic(res_km)
-                # datas_dict['ID_Road'] = row[0]
-                # datas_dict['Name'] = row[1]
-                # datas_dict['ID_Way'].append(row[2])
-                # datas_dict['Description'].append(row[3])  # направление движения по записи
-                # datas_dict['ID_High'].append(row[4])
-                # datas_dict['Description_uch1'].append(row[5])
-                # datas_dict['ID_Attribute'].append(row[6])
-                # datas_dict['L1-Начало'].append(row[7])
-                # datas_dict['L2-Конец'].append(row[8])
-                # datas_dict['ID_Param'].append(row[9])
-                # datas_dict['ValueParam'].append(row[10])
-                # datas_dict['ID_Type_Attr'].append(row[11])
-                # datas_dict['Item_Name'].append(row[12])
-        #icecream.ic(res_km)
-        #for key, value in dad_datas_dict.items():
-        #    print(key, value)
-        #return dad_datas_dict
-
-
+                km = ((0, param[-2]), (0, param[-1]))
+            coordinates = (*param[3::], *km)
+            #icecream.ic('DAD', coordinates)
+            if param[0] in res:
+                if param[1] in res.get(param[0]):
+                    if param[2] in res.get(param[0]).get(param[1]):
+                        res.get(param[0]).get(param[1]).get(param[2]).append(coordinates)
+                    else:
+                        res.get(param[0]).get(param[1]).update({param[2]: [coordinates]})
+                else:
+                    res.get(param[0]).update({param[1]: {param[2]: [coordinates]}})
+            else:
+                res.update({param[0]: {param[1]: {param[2]: [coordinates]}}})
+        sort_dict_binding(res)
+        #print([(i.keys()) for i in res.values() if isinstance(i, dict)])
+        return dict(sorted(res.items()))
 
     def close_db (self):
         self.db.close()
@@ -720,21 +693,20 @@ def databases ():
 
 def main ():
     import time
-    db = Query('OMSK_REGION_2023')  # FKU_VOLGO_VYATSK_1
+    db = Query('OMSK_REGION_2023')  # FKU_VOLGO_VYATSK_1 OMSK_REGION_2023 FKU_MOSKVA_NIGN_NOVGOROD_1
     list_roads = db.set_road_name()
     print(list_roads)
-    # data = db.get_dad_datas('P-254')  # Р-176 "Вятка" Чебоксары - Йошкар-Ола - Киров - Сыктывкар
 
     start = time.time()  # точка отсчета времени
-    #data_dad = db.get_dad_datas(list_roads[-3])
-    data_tp = db.get_tp_datas(list_roads[1])
-    #db.get_km_sign_list(list_roads[-3])
-    end = time.time() - start  # собственно время работы программы
-    print(f"{round(end, 1)} секунд")  # вывод времени
-    #print(data_test)
+    data_dad = db.get_dad_datas(list_roads[1])
+    #data_tp = db.get_tp_datas(list_roads[1])
 
-    # test(data)
-    print(data_tp)
+    end = time.time() - start  # время работы программы
+    print(f"{round(end, 1)} секунд")  # вывод времени
+    print(data_dad)
+    #print(data_tp.get('весь участок').get('Километровые знаки'))
+   #python -m cProfile -s cumulative some-db.py запускать в терминале для оценки времени работы скрипта
+
     db.close_db()
     # with open(rf'{data.get("название дороги","отчет")}.txt', 'w', encoding = 'utf-8') as file:
     #     for key, val in data.items():
